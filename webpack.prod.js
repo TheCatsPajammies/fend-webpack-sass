b3d6928546883bfb82d3731a07d177354d8611ed
TheCatsPajammies <67980898+TheCatsPajammies@users.noreply.github.com>
2021-01-11 13:02:27 -0600

    Loader Tutorial final steps

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,12 +5,20 @@ const HtmlWebPackPlugin = require("html-webpack-plugin")
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
+    output:{
+        libraryTarget: 'var',
+        library: 'Client',
+      },
     module: {
         rules: [
             {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             }
         ]
     },
@@ -18,6 +26,6 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
     ]
 }
